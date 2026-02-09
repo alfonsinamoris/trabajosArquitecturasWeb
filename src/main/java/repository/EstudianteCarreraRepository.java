598a23c01d53d7a42b86eda80c0cedc03ec57e97
alfonsinamoris <105081595+alfonsinamoris@users.noreply.github.com>
2026-02-09 11:57:40 -0300

    Delete src/main directory

--- src/main/java/repository/EstudianteCarreraRepository.java
+++
@@ -1,8 +0,0 @@
-package repository;
-
-
-
-public interface EstudianteCarreraRepository {
-    void insertarCSV(String rutaArchivo);
-    void matricularEstudiante(int dniEstudiante, int idCarrera);
-}
